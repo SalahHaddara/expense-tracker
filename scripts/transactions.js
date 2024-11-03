4d6031c64707c65136b316e3f8e913b0c1a648c2
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-03 19:22:44 +0200

    add save and add functions to transactions class

--- scripts/transactions.js
+++ scripts/transactions.js
@@ -2,4 +2,13 @@ class TransactionManager {
     constructor() {
         this.transactions = utils.getFromLocalStorage("transactions");
     }
+
+    save() {
+        utils.saveToLocalStorage("transactions", this.transactions);
+    }
+
+    addTransaction(transaction) {
+        this.transactions.push({...transaction, id: Date.now()});
+        this.save();
+    }
 }
